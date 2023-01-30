65742f3f07c4fe40b48f32e900eb9db49138cf7d
王小白 <731896502@qq.com>
2023-01-30 18:21:52 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-zinc-100 h-screen">
+    <title>小白的小小窝</title>
+    <header class="h-20 flex center items-center text-xl font-semibold bg-white">
+      <div class="w-28 text-center cursor-pointer hover:text-pink-400">随记</div>
+      <div class="w-28 text-center cursor-pointer hover:text-pink-400">关于我</div>
+    </header>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+<style></style>
