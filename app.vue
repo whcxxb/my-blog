27f0f106c1f8f072ce40119ce57d944ee06eb673
王小白 <731896502@qq.com>
2023-01-30 23:58:46 +0800

    up

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-zinc-100 h-screen">
+  <div class="bg-zinc-100 h-screen relative">
     <title>小白的小小窝</title>
-    <header class="h-20 flex center items-center text-xl font-semibold bg-white">
+    <header class="h-20 flex center items-center text-xl font-semibold bg-white w-full z-10 relative">
       <div class="w-28 text-center cursor-pointer hover:text-pink-400">随记</div>
       <div class="w-28 text-center cursor-pointer hover:text-pink-400">关于我</div>
     </header>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container mx-auto">
+    <div class="h-screen fixed top-0 left-0 opacity-60">
+      <video src="../assets/video/4.mp4"></video>
+    </div>
     内容
   </div>
 </template>
