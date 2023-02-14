6db049a05221ec0fe0fd7d94eb0498a75984b7f4
王小白 <731896502@qq.com>
2023-02-14 10:28:06 +0800

    up

--- app.vue
+++ app.vue
@@ -17,7 +17,7 @@
       class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between flex-wrap bg-white backdrop-blur-md bg-white/30 p-6"
     >
       <div class="flex items-center flex-shrink-0 mr-6">
-        <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        <span class="font-semibold text-xl tracking-tight">保持理智</span>
       </div>
       <div class="block lg:hidden">
         <button class="flex items-center px-3 py-2 border rounded border-teal-400 hover:text-white hover:border-white">
@@ -29,43 +29,43 @@
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4"> Features </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4"> Pricing </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white"> Blog </a>
+          <NuxtLink to="/about" class="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-pink-500">关于</NuxtLink>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4"> 随记 </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-white"> 音乐 </a>
         </div>
         <div>
-          <a
+          <!-- <a
             href="#"
             class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             >Download</a
-          >
+          > -->
         </div>
       </div>
     </nav>
     <div class="max-md:hidden transition-opacity opacity-60 fixed top-0 left-0 right-0 w-screen h-screen">
       <!-- <div class="max-md:hidden transition-opacity opacity-60 fixed top-0 left-0 right-0 container mx-auto"> -->
-      <video autoplay muted loop class="w-full max-h-full mx-auto" src="./assets/video/4.mp4 "></video>
+      <!-- <video autoplay muted loop class="w-full max-h-full mx-auto" src="./assets/video/4.mp4 "></video> -->
     </div>
     <NuxtPage class="container mx-auto mt-28 relative"></NuxtPage>
   </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
-const router = useRouter()
-const active = ref<number>(0)
-const menuList = reactive([
-  {
-    name: '随记',
-    path: '/'
-  },
-  {
-    name: '关于我',
-    path: '/about'
-  }
-])
-const selectChange = (index: number) => {
-  active.value = index
-  router.push(menuList[index].path)
-}
+// import { ref, reactive } from 'vue'
+// const router = useRouter()
+// const active = ref<number>(0)
+// const menuList = reactive([
+//   {
+//     name: '随记',
+//     path: '/'
+//   },
+//   {
+//     name: '关于我',
+//     path: '/about'
+//   }
+// ])
+// const selectChange = (index: number) => {
+//   active.value = index
+//   router.push(menuList[index].path)
+// }
 </script>
 <style></style>
